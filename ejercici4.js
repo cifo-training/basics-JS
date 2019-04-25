8d0c6113d6c1e0d651c718623fa210c4e461ddd8
Alumne <neollob@yahoo.es>
2019-04-25 10:40:52 +0200

    3rd commit

--- ejercici4.js
+++ ejercici4.js
@@ -1,3 +1,4 @@
 let e1 = prompt("Introdueix la teva edat");
-let ej=67-parseInt(e1);
-alert("Et queden "+ej+" anys per jubilar-te, ànims!")
+const ej=67;
+let epj=ej-parseInt(e1);
+alert("Et queden "+epj+" anys per jubilar-te, ànims!")
