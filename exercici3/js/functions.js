08db1eef9e497b0d306a6525ea7a4ce47164b27c
unknown <you@example.com>
2019-08-05 01:01:59 +0200

    exercici3inici commit

---
+++ exercici3/js/functions.js
@@ -0,0 +1,17 @@
+let texto = prompt('Introduce (broma geak) un "string", para el resto de los ciudadanos cuerdos, una cadena de texto, por favor');
+
+
+ console.log(texto);
+
+let azar = Math.random(String).toString(36); 
+console.log(azar);
+// not quite lo que busco
+
+
+ /* function myFunction() {
+  
+    document.getElementById('pintar').innerHTML = "Hola" + " " + juntos;
+    if(juntos === 'null' | juntos === 'null', 'null'){
+        document.getElementById('pintar').innerHTML = "Hola desconocido!";
+    }
+ }  */
