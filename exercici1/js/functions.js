aa11fd1892d9e1bb043d4dc23974fcc6a9af2347
unknown <you@example.com>
2019-08-05 00:01:35 +0200

    exercici2 commit

--- exercici1/js/functions.js
+++ exercici1/js/functions.js
@@ -1,1 +1,14 @@
+
 let x = "My favorite dessert is Jello";
+let y = x.indexOf('J');
+console.log(y);
+document.getElementById('boton1').onClick = function(){
+    myFunction()
+};
+function myFunction() {
+    
+     
+     document.getElementById('pintar').innerHTML = y;
+  } 
+
+
---
+++ exercici2/js/functions.js
@@ -0,0 +1,9 @@
+let nombre = prompt('¿ Cual es su nombre por favor?');
+let apellidos = prompt(' ¿y su primer apellido?');
+let juntos = nombre + " " + apellidos;
+ console.log(nombre + " " + apellidos);
+
+ function myFunction() {
+  
+    document.getElementById('pintar').innerHTML = "Hola" + " " + juntos;
+ } 
