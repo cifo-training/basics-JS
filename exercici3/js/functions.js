dcfb7e5bac2b2bf8b20daa64705595aba690ac91
unknown <you@example.com>
2019-08-06 11:30:59 +0200

    exercici3 commit

--- exercici3/js/functions.js
+++ exercici3/js/functions.js
@@ -3,15 +3,35 @@ let texto = prompt('Introduce (broma geak) un "string", para el resto de los ciu
 
  console.log(texto);
 
-let azar = Math.random(String).toString(36); 
-console.log(azar);
+/* let azar = Math.random(String).toString(36); 
+console.log(azar); */
 // not quite lo que busco
+// Debo generar YO el random y no cogerlo del string escrito en prompt
+
+//declaración de DONDE coge el string para generar el random
+
+let palabras = texto;
+// cuantos elementos quiero random
+let palabrasLength = " ";
+// donde coloco el valor de ese random
 
 
- /* function myFunction() {
+let azarPalabra = Math.floor(Math.random()* palabras.length);
+
+let randomPalabra = palabras.substring(azarPalabra, azarPalabra+1);
+alert ( 'La siguiente palabra,'+" "+ randomPalabra.toUpperCase() + " " + 'aparece en el texto introducido');
+
+ function azar(){
+// recorrido palabras pa generar el random
+for (i=0; i < palabrasLength; i++){
+    document.getElementById('pintar').innerHTML = azar();
+    
+}
+
+} 
+   function myFunction() {
   
-    document.getElementById('pintar').innerHTML = "Hola" + " " + juntos;
-    if(juntos === 'null' | juntos === 'null', 'null'){
-        document.getElementById('pintar').innerHTML = "Hola desconocido!";
-    }
- }  */
+    document.getElementById('pintar').innerHTML = randomPalabra;
+
+}
+ 
