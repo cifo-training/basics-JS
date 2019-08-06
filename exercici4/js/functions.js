80f131a4c513d164900330f162f757612bd4e385
unknown <you@example.com>
2019-08-06 17:49:07 +0200

    exercici4 commit

---
+++ exercici4/js/functions.js
@@ -0,0 +1,25 @@
+
+let edad = prompt("How old are you?");
+let edadJubilacion = 67;
+let jubiladoYa = (edadJubilacion - edad);
+console.log(edad);
+
+function jubilado(){
+    if(edad < edadJubilacion){
+        alert("You need still" +" "+ jubiladoYa +" " +" to become a goverment employ!")
+    }else{
+        alert("You are rather lucky,"+" "+ -jubiladoYa +" "+ "years enjoying your well earned peace of mind.")
+    }
+}
+document.getElementById('boton1').onClick = function(){
+    myFunction();
+}
+
+function myFunction() {
+    
+     
+     document.getElementById('pintar').innerHTML = jubilado();
+  } 
+
+
+
