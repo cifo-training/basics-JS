50c20c12f922adc98789b435ac2bfde35b050977
unknown <you@example.com>
2019-08-11 17:22:51 +0200

    checklist commit

--- exercici1/js/functions.js
+++ exercici1/js/functions.js
@@ -2,9 +2,10 @@
 let x = "My favorite dessert is Jello";
 let y = x.indexOf('J');
 console.log(y);
-document.getElementById('boton1').onClick = function(){
+/* document.getElementById('boton1').onClick = function(){
     myFunction()
-};
+}; */
+
 function myFunction() {
     
      
---
+++ exercici5/js/functions.js
@@ -0,0 +1,28 @@
+let bebidas = [
+    'cerveza',
+    'coca-cola',
+    'leche'
+];
+let edad = prompt('How old are you?');
+
+document.getElementById('boton1').onClick = function(){
+    myFunction();
+}
+function myFunction() {
+    
+     
+     document.getElementById('pintar').innerHTML = edad;
+  } 
+function addClassCheck(e){
+    if(e.checked){
+        e.classList.add("marked");
+    }else{
+        e.classList.remove("marked");
+    }
+    if(document.getElementsByClassName("marked").length > 1){
+        alert("Marca sólo una opción por favor.");
+        e.checked = false;
+        e.classList.remove("marked");
+    }
+}
+
