473f453fbc8f1f970a234ee430f7a2294ab96d29
unknown <you@example.com>
2019-11-29 11:40:33 +0100

    refactorización commit

--- exercici3/js/functions.js
+++ exercici3/js/functions.js
@@ -1,35 +1,35 @@
-let texto = prompt('Introduce letras de la A a la Z');
 
 
- console.log(texto);
-
-/* let azar = Math.random(String).toString(36); 
-console.log(azar); */
-// not quite lo que busco
-// Debo generar YO el random y no cogerlo del string escrito en prompt
-
-//declaración de DONDE coge el string para generar el random
-
-let palabras = texto;
-// cuantos elementos quiero random
-let palabrasLength = " ";
-// donde coloco el valor de ese random
-
-
-let azarPalabra = Math.floor(Math.random()* palabras.length);
-
- function azar(){
+/*  function azar(){
 // recorrido palabras pa generar el random
 for (i=0; i < palabrasLength; i++){
     document.getElementById('pintar').innerHTML = azar();
-    console.log(azar);
+    
 }
 
-} 
+}  */
    function myFunction() {
-    let randomPalabra = palabras.substring(azarPalabra, azarPalabra+1);
-    document.getElementById('pintar2').innerHTML = ( 'La letra introducida,'+" "+ randomPalabra.toUpperCase() + " " + 'aparece en el texto generado al azar');
-    document.getElementById('pintar1').innerHTML = randomPalabra.toUpperCase();
+    let texto = prompt('Introduce una palabra de la A a la Z').toLowerCase();
+
+    document.getElementById('pintar1').innerHTML = (`La letra inscrita es la ${texto}`);
+
+   // Debo generar YO el random y no cogerlo del string escrito en prompt
+   
+   //declaración de DONDE coge el string para generar el random
+   
+   let palabras = "ABCÇDEFGHIJKLMNOPQRSTUVWXTZabcçdefghiklmnopqrstuvwxyz";
+   // cuantos elementos quiero random
+  // let palabrasLength = " ";
+   // donde coloco el valor de ese random
+
+   let azarPalabra = Math.floor(Math.random()* palabras.toLowerCase().length);
+   
+   let randomPalabra = palabras.toLowerCase().substring(azarPalabra, azarPalabra+1);
+   document.getElementById('pintar2').innerHTML = (`La letra generada es la ${randomPalabra}`);
+   //comparamos los dos valores
+ 
+   let compara = texto.includes(randomPalabra);
+    document.getElementById('pintar').innerHTML = compara;
 
 }
 /* Finalmente he tenido que buscar el random en el prompt. pequeño descanso y quiero hacerlo con un random */
--- exercici8/js/functions.js
+++ exercici8/js/functions.js
@@ -9,12 +9,15 @@ let same =  x.length == nameDriver.length; */
 
 const myFunction = ()=> {
     larger = x.length > nameDriver.length?
+
     document.getElementById('pintar').innerHTML = (`You ${x} got a larger name, it has ${x.length} characters`):
     document.getElementById('pintar').innerHTML = (`I, ${nameDriver} got the larger name, I've got ${nameDriver.length} characters`);
     same = x.length == nameDriver.length?
-    document.getElementById('pintar').innerHTML = (`You ${x} and I ${nameDriver} gort the same number of characters`):
+
+    document.getElementById('pintar').innerHTML = (`You ${x} and I ${nameDriver} got the same number of characters`):
      false;
     none = x.length == ""?
+    
     document.getElementById('pintar').innerHTML = (`No name, no game`):
      false;
 };
